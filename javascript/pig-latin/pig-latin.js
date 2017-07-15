bef14a62b601d5043e45184226d6ba56a01575e7
Chad Kreutzer <kanthalion@gmail.com>
2017-07-15 10:35:27 +0000

    finished pig-latin

---
+++ javascript/pig-latin/pig-latin.js
@@ -0,0 +1,11 @@
+const translate = (string) => {
+    const V = /^[aeiouy]/;
+    const CorQ = /(\w?qu|[^aeiouy\s]+)(\w+)/;
+    
+    return string
+        .split(" ")
+        .map(word => `${(V.test(word)) ? word : word.replace(CorQ, "$2$1")}ay`)
+        .join(" ");
+};
+
+module.exports = { translate };
